802b27178929abf7349b96c3a1414144126f5813
plantainbread <zyuanboost2026@gmail.com>
2025-03-03 00:28:44 -0800

    Translate parts of AbstractFunctionLayer

---
+++ layer/AbstractFunctionLayer.py
@@ -0,0 +1,19 @@
+class AbstractFunctionLayer(Layer):
+    emittedSubtask
+    subtask
+
+    def __init__(self):
+        self.emittedSubtask = True
+        self.subtask = None
+
+    def isTaskDone():
+        return emittedSubtask
+    
+    def update(iter(completed)):
+        if(emittedSubtask):
+            # Unsure how to translate the illegal state exception part to python.
+            raise Exception("Illegal state exception")
+        emittedTask = True
+        # return 
+            
+
