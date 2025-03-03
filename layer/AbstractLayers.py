bde01f095432094212f0b5f8adc847beeeb097f8
plantainbread <zyuanboost2026@gmail.com>
2025-03-03 01:00:33 -0800

    Add a bit of TopLayerSequence

--- layer/AbstractLayers.py
+++ layer/AbstractLayers.py
@@ -8,10 +8,10 @@ class AbstractFunctionLayer(Layer):
         self.emittedSubtask = True
         self.subtask = None
 
-    def isTaskDone():
+    def isTaskDone(self):
         return emittedSubtask
     
-    def update(iter(completed)):
+    def update(self, iter(completed)):
         if(emittedSubtask):
             # Unsure how to translate the illegal state exception part to python.
             raise IllegalStateException("Something")
@@ -21,16 +21,17 @@ class AbstractFunctionLayer(Layer):
 
 class AbstractQueuedLayer(Layer):
     iter(subtaskIter)
-    def __init__():
+    def __init__(self):
         subtaskIter = None
 
-    def setup(setupInfo):
+    def setup(self, setupInfo):
         pass
 
-    def isTaskDone():
-        return subtaskIter == None or next(subtaskIter) == None
+    def isTaskDone(self):
+        # unsure how to get hasNext() for iterator.
+        return subtaskIter == None
 
-    def update(Iter(completed)):
+    def update(self, ter(completed)):
         # placeholder
         pass
 
---
+++ layer/TopLayerSequence.py
@@ -0,0 +1,19 @@
+class TopLayerSequence(Layer):
+    layers = []
+
+    layersIter = iter(layers)
+
+    layer
+
+    logger
+
+    def __init__(self, layers):
+        self.layers = layers
+
+    def isTaskDone(self):
+        # not sure how to get hasNext() for iterator.
+        return layer.isTaskDone()
+
+    def setup(self, setupInfo):
+        #placeholder
+        pass
