0d9e57f8c29464455d027c6259fc841bd6fa6042
plantainbread <zyuanboost2026@gmail.com>
2025-03-05 09:21:17 -0800

    Add some parts of Mat2

---
+++ matrix/Mat2.py
@@ -0,0 +1,33 @@
+import math
+from Vec2 import Vec2
+
+class Mat2:
+    def __init__(self, m00, m10, m01, m11):
+        self.mat = [m00, m10, m01, m11]
+
+        def fromAngle(angle):
+            return Mat2(math.cos(angle), -math.sin(angle), math.sin(angle), math.cos(angle))
+
+        def mulMat(other):
+            return Mat2(
+                    mat[0] * other.mat[0] + mat[1] * other.mat[2],
+                    mat[0] * other.mat[1] + mat[1] * other.mat[3],
+                    mat[2] * other.math[0] + mat[3] * other.mat[2],
+                    mat[2] * other.mat[1] + mat[3] * other.mat[3]
+                    )
+
+        def mulVec2(other):
+            return Vec2(
+                    mat[0] * other.getX() + mat[1] * other.getY(),
+                    mat[2] * other.getX() + mat[3] * other.getY()
+                    )
+
+        def mulScalar(other):
+            return Mat2(
+                    mat[0] * other,
+                    mat[1] * other,
+                    mat[2] * other,
+                    mat[3] * other
+                    )
+
+
