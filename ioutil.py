9095bf393bc9505945fc3efb18069e7b6858c49f
STEAM_OASYS <steamoasys@gmail.com>
2025-03-07 09:37:27 -0800

    Finish ioutil

--- ioutil.py
+++ ioutil.py
@@ -1,5 +1,13 @@
+from struct import pack
+
 def write_flexible_string(buf, string):
-    pass
+    if string == None:
+        buf.append(0)
+    else:
+        buf.append(min(255, len(string)))
+        buf.extend(string.encode('ascii'))
+        if len(string) > 255:
+            buf.append(0)
 
 def write_double(buf, double):
-    pass
+    buf.extend(pack('!d', double))
