b7305354b0886b8f1f2e4a9b3f5b1c1f91821ddc
plantainbread <zyuanboost2026@gmail.com>
2025-03-12 09:04:12 -0700

    Rename some method names

--- matrix/Mat2.py
+++ matrix/Mat2.py
@@ -5,10 +5,10 @@ class Mat2:
     def __init__(self, m00, m10, m01, m11):
         self.mat = [m00, m10, m01, m11]
 
-        def fromAngle(angle):
+        def from_angle(angle):
             return Mat2(math.cos(angle), -math.sin(angle), math.sin(angle), math.cos(angle))
 
-        def mulMat(other):
+        def mul_mat(other):
             return Mat2(
                     mat[0] * other.mat[0] + mat[1] * other.mat[2],
                     mat[0] * other.mat[1] + mat[1] * other.mat[3],
@@ -16,13 +16,13 @@ class Mat2:
                     mat[2] * other.mat[1] + mat[3] * other.mat[3]
                     )
 
-        def mulVec2(other):
+        def mul_vec2(other):
             return Vec2(
                     mat[0] * other.getX() + mat[1] * other.getY(),
                     mat[2] * other.getX() + mat[3] * other.getY()
                     )
 
-        def mulScalar(other):
+        def mul_scalar(other):
             return Mat2(
                     mat[0] * other,
                     mat[1] * other,
