7c991ff4f4d27e8f2d53dbc3ce8fbc94371f3734
Eddy Meals <edimea211@gmail.com>
2025-03-31 23:31:07 -0700

    Use project conventions
    Use purpose-made classes instead of dictionaries.
    Use underscore naming scheme for "private" fields.
    Replace numpy with matrix library.

--- layer/pathfinding.py
+++ layer/pathfinding.py
@@ -1,4 +1,6 @@
-import numpy as np
+from os import time
+from math import inf
+from task import MoveToFieldTask, UnsupportedTaskError
 
 class PathfindingLayer:
     GOAL_COMPLETE_EPSILON = 0.01
@@ -11,45 +13,39 @@ class PathfindingLayer:
     TARGET_ANGLE_SMOOTHING_K = 1
     CLEARANCE_STEP = 0.05
 
-    def __init__(self):
-        self.goal = np.eye(3)
-        self.obstacles = []
-        self.currentTrajectory = None
-        self.lastCalcTime = 0
-        self.initialTransform = np.eye(3)
-        self.initialVelocity = np.eye(3)
-        self.localizer = None
-
     def setup(self, setup_info):
-        self.localizer = setup_info.get('localizer')
+        self._obstacles = []
+        self._localizer = setup_info.get_localizer()
+        self._initial_transform = None
 
     def is_task_done(self):
-        delta = np.linalg.inv(self.get_transform()) @ self.goal
-        translation_len = np.linalg.norm(delta[:2, 2])
-        direction_angle = np.arctan2(delta[1, 0], delta[0, 0])
-        return translation_len < self.GOAL_COMPLETE_EPSILON and abs(direction_angle) < self.GOAL_COMPLETE_EPSILON
+        delta = self._get_transform().inv().mul(self._goal)
+        return (delta.get_translation().len() < self.GOAL_COMPLETE_EPSILON
+            and delta.get_direction().get_angle() < self.GOAL_COMPLETE_EPSILON)
 
     def update(self, completed):
-        now = self.current_time()
-        if now - self.lastCalcTime > self.CALCULATE_INTERVAL:
-            self.calculate_path()
+        now = time()
+        if now - self._last_calc_time > self.CALCULATE_INTERVAL:
+            self._calculate_path()
             self.lastCalcTime = now
-        return [(self.currentTrajectory.axial, -self.currentTrajectory.lateral, self.currentTrajectory.yaw)]
+        return iter([HolonomicDriveTask(
+            self._current_trajectory.axial,
+            -self._current_trajectory.lateral,
+            self._current_trajectory.yaw
+        )])
 
     def accept_task(self, task):
-        if isinstance(task, dict) and task.get('type') == 'MoveToFieldTask':
-            self.goal = task.get('goal_transform')
-            self.lastCalcTime = self.current_time() - self.CALCULATE_INTERVAL
+        if isinstance(task, MoveToFieldTask):
+            self._goal = task.get_goal_transform()
+            self.lastCalcTime = -inf
+        else:
+            raise UnsupportedTaskError(self, task)
 
     def evaluate_trajectory(self, trajectory):
-        target_angle_score = self.evaluate_target_angle(trajectory) * self.TARGET_ANGLE_COEFF
-        clearance_score = self.evaluate_clearance(trajectory) * self.CLEARANCE_COEFF
-        speed_score = self.evaluate_speed(trajectory) * self.SPEED_COEFF
+        target_angle_score = self._evaluate_target_angle(trajectory) * self.TARGET_ANGLE_COEFF
+        clearance_score = self._evaluate_clearance(trajectory) * self.CLEARANCE_COEFF
+        speed_score = self._evaluate_speed(trajectory) * self.SPEED_COEFF
         return target_angle_score + clearance_score + speed_score
-
-    def current_time(self):
-        import time
-        return time.time()
 
 class Trajectory:
     def __init__(self, axial, lateral, yaw):
