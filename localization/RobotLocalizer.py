bfdfa3691e6bacbabdc3c5813192a8e957889a85
plantainbread <zyuanboost2026@gmail.com>
2025-04-06 10:34:00 -0700

    Translate RobotLocalizer

--- localization/RobotLocalizer.py
+++ localization/RobotLocalizer.py
@@ -1,1 +1,25 @@
-import 
+from Localization import *
+
+from abc import ABC
+from abc import abstractmethod
+
+class RobotLocalizer(ABC):
+    @abstractmethod
+    def invalidate_cache():
+        pass
+
+    @abstractmethod
+    def register_source(source: LocalizationData):
+        pass
+
+    @abstractmethod
+    def resolve_transform():
+        pass
+
+    @abstractmethod
+    def resolve_position():
+        pass
+
+    @abstractmethod
+    def resolve_rotation():
+        pass
