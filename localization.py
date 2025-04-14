0a864fff2d205ad00bcd350271a232695da34db5
STEAM_OASYS <steamoasys@gmail.com>
2025-04-14 12:48:04 -0700

    Merge localization package to one file

--- localization.py
+++ localization.py
@@ -1,6 +1,9 @@
-from abc import ABC, abstractmethod
-from matrix import Vec2
+from abc import ABC
+from abc import abstractmethod
 from functools import reduce
+from layer import Layer
+from matrix import Vec2
+from task.sensory import LocalizationTask
 import math
 
 class LocalizationData(ABC):
@@ -37,7 +40,7 @@ class LocalizationData(ABC):
         pass
 
     
-class AbstractFinDiffLocalization(LocalizationData):
+class AbstractFinDiffLocalizationData(LocalizationData):
     def __init__(self, epsilon):
         self._epsilon = epsilon
 
@@ -90,6 +93,7 @@ class AbstractFinDiffLocalization(LocalizationData):
         # Question: in the code it just says get_rotational_probability, does it need to be with respect to x or y or some other thing?
         return (self.get_rotational_probability(rot + self._epsilon) - self.get_rotational_probability(rot)) / self._epsilon * ignore_root_factor
 
+
 class LocalizationSource(ABC):
     @abstractmethod
     def canLocalizePosition():
@@ -125,19 +129,53 @@ class RobotLocalizer(ABC):
     def resolveRotation():
         pass
 
-    class SqFalloffLocalizationData(AbstractFinDiffLocalization):
-        def __init__(self, _epsilon, transform, accuracy, _position_precision, _rotation_precision):
-            super().__init__(_epsilon)
-            self._transform = transform
-            self._accuracy = accuracy
-            self._position_precision = _position_precision
-            self._rotation_precision = _rotation_precision
 
-        def get_position_probability(self, pos):
-            diff = self._transform.get_translation().mul(-1).add(pos)
-            return self._accuracy / ((diff.dot(diff) * self._position_precision) + 1)
+class SqFalloffLocalizationData(AbstractFinDiffLocalization):
+    def __init__(self, _epsilon, transform, accuracy, _position_precision, _rotation_precision):
+        super().__init__(_epsilon)
+        self._transform = transform
+        self._accuracy = accuracy
+        self._position_precision = _position_precision
+        self._rotation_precision = _rotation_precision
 
-        def get_rotation_probability(self, rot):
-            diff = rot - self._transform.get_direction().get_angle()
-            return self._accuracy / (diff * diff * self._rotation_precision + 1)
+    def get_position_probability(self, pos):
+        diff = self._transform.get_translation().mul(-1).add(pos)
+        return self._accuracy / ((diff.dot(diff) * self._position_precision) + 1)
 
+    def get_rotation_probability(self, rot):
+        diff = rot - self._transform.get_direction().get_angle()
+        return self._accuracy / (diff * diff * self._rotation_precision + 1)
+
+
+class RobotLocalizer(Layer):
+    def setup(self, setup_info):
+        setup_info.add_update_listener(lambda: self.invalidate_cache())
+
+    def get_input_tasks(self):
+        return set()
+
+    def get_output_tasks(self):
+        return {LocalizationTask}
+
+    def complete_task(self, task):
+        if task == self._emitted_task:
+            self._should_emit = True
+
+    def process(self, ctx):
+        if self._should_emit:
+            ctx.emit_subtask(LocalizationTask(self._resolve_transform()))
+
+    def accept_task(self):
+        raise TypeError
+
+    @abstractmethod
+    def resolve_transform(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def invalidate_cache():
+        pass
+
+    @abstractmethod
+    def register_source(self, source: LocalizationSource):
+        pass
