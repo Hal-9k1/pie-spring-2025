1925c25efc293fada09d9f2d0c196762d4f7ad29
Eddy Meals <edimea211@gmail.com>
2025-06-12 17:46:19 -0700

    Change tpl matching back to own mp chunk, add transform and convolve to accel

--- resources/ImageG8-accel.c
+++ resources/ImageG8-accel.c
@@ -26,10 +26,13 @@ typedef struct
   double data[6];
 } Mat3;
 
-static void mulMat3Vec2d(Mat3 *pMat, Vec2d vec, Vec2d *pResult)
+static Vec2d mulMat3Vec2d(Mat3 *pMat, Vec2d vec)
 {
-  pResult->x = pMat->data[0] * vec.x + pMat->data[1] * vec.y + pMat->data[2];
-  pResult->y = pMat->data[3] * vec.x + pMat->data[4] * vec.y + pMat->data[5];
+  Vec2d result = {
+    pMat->data[0] * vec.x + pMat->data[1] * vec.y + pMat->data[2],
+    pMat->data[3] * vec.x + pMat->data[4] * vec.y + pMat->data[5]
+  };
+  return result;
 }
 
 static double implicitRow[3] = { 0, 0, 1 };
@@ -38,7 +41,7 @@ static double rowColDot(
   Mat3 *pMatA,
   int row,
   Mat3 *pMatB,
-  int col,
+  int col
 ) {
   int aStart = row * 3;
   return pMatA->data[aStart] * pMatB->data[col]
@@ -51,20 +54,64 @@ static void mulMat3Mat3(Mat3 *pMatA, Mat3 *pMatB, Mat3 *pResult)
   for (int i = 0; i < sizeof(pMatA->data) / sizeof(pMatA->data[0]); ++i)
   {
     div_t d = div(i, 3);
-    result[i] = rowColDot(a, d.quot, b, d.rem);
+    pResult->data[i] = rowColDot(pMatA, d.quot, pMatB, d.rem);
   }
 }
 
-typedef void (*Multiprocessable)(int start, int end, int id, void *pArg);
+static double clampd(double value, double min, double max)
+{
+  double temp = value > max ? max : value;
+  return temp < min ? min : temp;
+}
 
-typedef struct
+static int clampi(int value, int min, int max)
+{
+  int temp = value > max ? max : value;
+  return temp < min ? min : temp;
+}
+
+static uint8_t getImageG8(ImageG8 img, int x, int y)
+{
+  return img.pData[img.size.x * y + x];
+}
+
+static double getInterpolatedImageG8(ImageG8 img, double x, double y)
+{
+  double modfDummy;
+  double xClamped = clampd(x, 0, 1) * (img.size.x - 1);
+  double xFrac = modf(xClamped, &modfDummy);
+  double xComp = 1 - xFrac;
+  int xHighUnclamped = xClamped + 1;
+  int xHigh = xHighUnclamped > img.size.x ? img.size.x : xHighUnclamped;
+  int xLow = xClamped;
+
+  double yClamped = clampd(y, 0, 1) * (img.size.y - 1);
+  double yFrac = modf(yClamped, &modfDummy);
+  double yComp = 1 - yFrac;
+  int yHighUnclamped = yClamped + 1;
+  int yHigh = yHighUnclamped > img.size.y ? img.size.y : yHighUnclamped;
+  int yLow = yClamped;
+
+  return (
+    xComp * yComp * getImageG8(img, xLow, yLow) +
+    xFrac * yComp * getImageG8(img, xHigh, yLow) +
+    xComp * yFrac * getImageG8(img, xLow, yHigh) +
+    xFrac * yFrac * getImageG8(img, xHigh, yHigh)
+  );
+}
+
+typedef struct MultiprocessInvocation_t MultiprocessInvocation;
+
+typedef void (*Multiprocessable)(MultiprocessInvocation *pInvoc);
+
+struct MultiprocessInvocation_t
 {
   Multiprocessable func;
   int start;
   int end;
   int id;
-  void *pArg;
-} MultiprocessInvocation;
+  void *pUserdata;
+};
 
 static void *multiprocessThread(void *pArg)
 {
@@ -78,7 +125,8 @@ static int multiprocess(Multiprocessable func, int range, int threads, void *pAr
 {
   if (threads == 1)
   {
-    func(0, range, 0, pArg);
+    MultiprocessInvocation invoc = { func, 0, range, 0, pArg };
+    func(&invoc);
     return 0;
   }
   int itemsPerChunk = range / threads;
@@ -100,7 +148,7 @@ static int multiprocess(Multiprocessable func, int range, int threads, void *pAr
     pInvoc->start = itemsPerChunk * i;
     pInvoc->end = (i == threads - 1) ? range : pInvoc->start + itemsPerChunk;
     pInvoc->id = i;
-    pInvoc->pArg = pArg;
+    pInvoc->pUserdata = pArg;
     if (pthread_create(pThreads + i, &attributes, multiprocessThread, pInvoc))
     {
       return 2;
@@ -120,51 +168,13 @@ static int multiprocess(Multiprocessable func, int range, int threads, void *pAr
 
 typedef struct
 {
-  Vec2i pos;
-  Vec2i imgSize;
-  void *pUniforms;
-  uint8_t *pOut;
-} ShaderInvocation;
-
-typedef void (*Shader)(ShaderInvocation *pInvoc);
-
-typedef struct
-{
-  ImageG8 img;
-  Shader shader;
-  void *pUniforms;
-} ShadeInfo;
-
-static void shadeImageChunk(MultiprocessInvocation *pInvoc)
-{
-  ShadeInfo *pInfo = pInvoc->pUserdata;
-  Vec2i size = pInfo->img.size:
-  ShaderInvocation shaderInvoc = {{0, 0}, size, pInfo->pUniforms, pInfo->img.data};
-  for (int i = pArg->start; i < pArg->end; ++i)
-  {
-    div_t d = div(i, size.x);
-    shaderInvoc.pos.y = d.quot;
-    shaderInvoc.pos.x = d.rem;
-    ++shaderInvoc.pOut;
-    pInfo->shader(&shaderInvoc);
-  }
-}
-
-int shadeImage(ImageG8 img, Shader shader, void *pUniforms, int threads)
-{
-  ShadeInfo info = { img, shader, pUniforms };
-  return multiprocess(shadeImageChunk, img.size.x * img.size.y, threads, &info);
-}
-
-typedef struct
-{
   uint32_t *pResults;
   uint32_t *pData;
 } NumberAggregateInfo;
 
 static void minChunk(MultiprocessInvocation *pInvoc)
 {
-  NumberAggregateInfo *pInfo = pInvoc->pArg;
+  NumberAggregateInfo *pInfo = pInvoc->pUserdata;
   uint32_t result = 0;
   for (int i = pInvoc->start; i < pInvoc->end; ++i)
   {
@@ -175,7 +185,7 @@ static void minChunk(MultiprocessInvocation *pInvoc)
 
 static void maxChunk(MultiprocessInvocation *pInvoc)
 {
-  NumberAggregateInfo *pInfo = pInvoc->pArg;
+  NumberAggregateInfo *pInfo = pInvoc->pUserdata;
   uint32_t result = 0;
   for (int i = pInvoc->start; i < pInvoc->end; ++i)
   {
@@ -188,8 +198,8 @@ static int parallelAggregate(
   Multiprocessable func,
   int count,
   uint32_t *pData,
-  int threads,
-  uint32_t *pResult
+  uint32_t *pResult,
+  int threads
 ) {
   uint32_t *pAggResults = malloc(threads * sizeof(uint32_t));
   if (!pAggResults)
@@ -204,7 +214,8 @@ static int parallelAggregate(
   }
   aggInfo.pResults = pResult;
   aggInfo.pData = pAggResults;
-  func(0, threads, 0, &aggInfo);
+  MultiprocessInvocation invoc = { func, 0, threads, 0, &aggInfo };
+  func(&invoc);
   free(pAggResults);
   return 0;
 }
@@ -219,7 +230,7 @@ typedef struct
 
 static void normalizeErrorChunk(MultiprocessInvocation *pInvoc)
 {
-  NormalizeInfo *pInfo = pInvoc->pArg;
+  NormalizeInfo *pInfo = pInvoc->pUserdata;
   for (int i = pInvoc->start; i < pInvoc->end; ++i)
   {
     pInfo->pResults[i] = (pInfo->pData[i] - pInfo->low) * pInfo->scale;
@@ -231,29 +242,32 @@ typedef struct
   ImageG8 image;
   ImageG8 template;
   ImageG8 *pMask;
+  Vec2i resultSize;
   double errScalingFac;
   uint32_t *pBuf;
-} TemplateMatchUniform;
+} TemplateMatchInfo;
 
-static void templateMatchShader(ShaderInvocation *pInvoc)
+static void templateMatchChunk(MultiprocessInvocation *pInvoc)
 {
-  TemplateMatchUniform *pUniform = pInvoc->pUniform;
-  int ay = pInvoc->pos.x;
-  int ax = pInvoc->pos.y;
-  Vec2i imgSize = pUniform->image.size;
-  Vec2i tplSize = pUniform->template.size;
-  long err = 0;
-  for (int by = 0; by < tplSize.y; ++by)
+  TemplateMatchInfo *pInfo = pInvoc->pUserdata;
+  for (int i = pInvoc->start; i < pInvoc->end; ++i)
   {
-    for (int bx = 0; bx < tplSize.size.x; ++bx)
+    div_t imageCoords = div(i, pInfo->resultSize.x);
+    int ay = imageCoords.quot;
+    int ax = imageCoords.rem;
+    long err = 0;
+    for (int by = 0; by < pInfo->template.size.y; ++by)
     {
-      int a = pUniform->image.pData[imgSize.x * (ay + by) + ax + bx];
-      int b = pUniform->template.pData[tplSize.x * by + bx];
-      int maskFac = pUniform->pMask ? pUniform->pMask->pData[pInfo->pMask->size.x * by + bx] : 255;
-      err += abs(a - b) * maskFac;
+      for (int bx = 0; bx < pInfo->template.size.x; ++bx)
+      {
+        int a = getImageG8(pInfo->image, ax + bx, ay + by);
+        int b = getImageG8(pInfo->template, bx, by);
+        int maskFac = pInfo->pMask ? getImageG8(*pInfo->pMask, bx, by) : 255;
+        err += abs(a - b) * maskFac;
+      }
     }
+    pInfo->pBuf[i] = ceil(err * pInfo->errScalingFac);
   }
-  pUniform->pBuf[i] = ceil(err * pUniform->errScalingFac);
 }
 
 static int templateMatchImpl(
@@ -269,10 +283,11 @@ static int templateMatchImpl(
   {
     return 1;
   }
-  TemplateMatchUniform uniform = {
+  TemplateMatchInfo templateInfo = {
     image,
     template,
     pMask,
+    out.size,
     // Divide by template area to get average error across template
     // Divide by 2 because difference could be from -255 to 255, brings range to 0-255
     // Divide by 255 from mask
@@ -281,7 +296,7 @@ static int templateMatchImpl(
     1.0 / (template.size.x * template.size.y) / 2 / 255 / 255 * (1L << 32),
     pBuf
   };
-  int err = shadeImage(out, templateMatchShader, &uniform, threads);
+  int err = multiprocess(templateMatchChunk, totalPx, threads, &templateInfo);
   if (err)
   {
     return err;
@@ -315,14 +330,9 @@ static int templateMatchImpl(
   return 0;
 }
 
-const char *templateMatch(
-  ImageG8 image,
-  ImageG8 template,
-  ImageG8 *pMask,
-  ImageG8 out,
-  int threads
-) {
-  switch (templateMatchImpl(image, template, pMask, out, threads))
+const char *errCodeToCStr(int code)
+{
+  switch (code)
   {
     case 0:
       return NULL;
@@ -335,42 +345,141 @@ const char *templateMatch(
     default:
       return "Unknown error";
   }
+
+}
+
+const char *templateMatch(
+  ImageG8 image,
+  ImageG8 template,
+  ImageG8 *pMask,
+  ImageG8 out,
+  int threads
+) {
+  return errCodeToCStr(templateMatchImpl(image, template, pMask, out, threads));
+}
+
+typedef struct
+{
+  int x;
+  int y;
+  Vec2i imgSize;
+  void *pUniforms;
+  uint8_t *pOut;
+} ShaderInvocation;
+
+typedef void (*Shader)(ShaderInvocation *pInvoc);
+
+typedef struct
+{
+  ImageG8 img;
+  Shader shader;
+  void *pUniforms;
+} ShadeInfo;
+
+static void shadeImageChunk(MultiprocessInvocation *pInvoc)
+{
+  ShadeInfo *pInfo = pInvoc->pUserdata;
+  Vec2i size = pInfo->img.size;
+  ShaderInvocation shaderInvoc = {0, 0, size, pInfo->pUniforms, pInfo->img.pData};
+  for (int i = pInvoc->start; i < pInvoc->end; ++i)
+  {
+    div_t d = div(i, size.x);
+    shaderInvoc.y = d.quot;
+    shaderInvoc.x = d.rem;
+    ++shaderInvoc.pOut;
+    pInfo->shader(&shaderInvoc);
+  }
+}
+
+static int shadeImage(ImageG8 img, Shader shader, void *pUniforms, int threads)
+{
+  ShadeInfo info = { img, shader, pUniforms };
+  return multiprocess(shadeImageChunk, img.size.x * img.size.y, threads, &info);
 }
 
 typedef struct
 {
   ImageG8 img;
-  double invTfm[9];
+  Mat3 invTfm;
   Vec2i offset;
-  Vec2i resultSize;
   uint8_t fill;
-  bool interpolate;
+  int shouldInterpolate;
 } TransformUniform;
 
-void transformShader(ShaderInvocation *pInvoc)
+static void transformShader(ShaderInvocation *pInvoc)
 {
-  TransformUniform *pUniform = pInvoc->pUniform;
-  Vec2d tfmedPos = { pInvoc->pos.x + pUniform->offset.x, pInvoc->pos.y + pUniform->offset.y };
-  Vec2d pos = mulMat3Vec2d(pUniform->invTfm, tfmedPos);
+  TransformUniform *pUniform = pInvoc->pUniforms;
+  Vec2d tfmedPos = { pInvoc->x + pUniform->offset.x, pInvoc->y + pUniform->offset.y };
+  Vec2d pos = mulMat3Vec2d(&pUniform->invTfm, tfmedPos);
   Vec2i imgSize = pUniform->img.size;
-  if (pos.x < 0 || pox.x > imgSize.x || pos.y < 0 || pos.y > imgSize.y)
+  if (pos.x < 0 || pos.x > imgSize.x || pos.y < 0 || pos.y > imgSize.y)
   {
     *pInvoc->pOut = pUniform->fill;
   }
-  if (pUniform->interpolate)
+  if (pUniform->shouldInterpolate)
   {
-    *pInvoc->pOut = getInterpolatedImageG8(pos.x / imgSize.x, pos.y / imgSize.y);
+    *pInvoc->pOut = getInterpolatedImageG8(pUniform->img, pos.x / imgSize.x, pos.y / imgSize.y);
   }
   else
   {
-    *pInvoc->pOut = pUniform->img.data[imgSize.x * pos.y + pos.x];
+    *pInvoc->pOut = getImageG8(pUniform->img, pos.x, pos.y);
   }
+}
+
+const char *transform(
+  ImageG8 img,
+  Mat3 invTfm,
+  Vec2i offset,
+  uint8_t fill,
+  int shouldInterpolate,
+  ImageG8 result,
+  int threads
+) {
+  TransformUniform uniform = {
+    img,
+    invTfm,
+    offset,
+    fill,
+    shouldInterpolate
+  };
+  return errCodeToCStr(shadeImage(result, transformShader, &uniform, threads));
 }
 
 typedef struct
 {
+  ImageG8 img;
   ImageG8 kernel;
-  uint8_t fill;
-} ConvolveUniform;
+  Vec2i kernelCenter;
+} ConvolutionUniform;
 
-static void
+static void convolutionShader(ShaderInvocation *pInvoc)
+{
+  ConvolutionUniform *pUniform = pInvoc->pUniforms;
+  double value = 0;
+  for (int ky = 0; ky < pUniform->kernel.size.y; ++ky)
+  {
+    int sy = clampi(pInvoc->y - pUniform->kernelCenter.y + ky, pInvoc->imgSize.y - 1, 0);
+    for (int kx = 0; kx < pUniform->kernel.size.x; ++kx)
+    {
+      int sx = clampi(pInvoc->x - pUniform->kernelCenter.x + kx, pInvoc->imgSize.x - 1, 0);
+      int sample = getImageG8(pUniform->img, sx, sy);
+      double weight = (double)getImageG8(pUniform->kernel, kx, ky) / 128 - 1;
+      value += sample * weight;
+    }
+  }
+  *pInvoc->pOut = clampi(value, 0, 255);
+}
+
+const char *convolve(
+  ImageG8 img,
+  ImageG8 kernel,
+  ImageG8 result,
+  int threads
+) {
+  ConvolutionUniform uniform = {
+    img,
+    kernel,
+    { kernel.size.x / 2, kernel.size.y / 2 }
+  };
+  return errCodeToCStr(shadeImage(result, convolutionShader, &uniform, threads));
+}
